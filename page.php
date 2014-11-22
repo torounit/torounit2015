ff89d16cddb5e2e6bb76969503fe805dad8502d5
Hiroshi Urabe <mail@torounit.com>
2014-11-22 22:38:26 +0900

    pageのbugfix

--- page.php
+++ page.php
@@ -1,7 +1,7 @@
 <?php get_header();?>
     <div class="l-row l-row_content">
         <main class="l-main" itemscope itemtype="http://schema.org/WebPageElement" itemprop="mainContentOfPage">
-            <?php if(have_posts()):?>
+            <?php if(have_posts()):the_post();?>
 
                 <article <?php post_class("p-article c-card");?>  itemscope itemtype="http://schema.org/Article">
                     <header class="p-article__header">
