e8deeb2288e61a61b77c17ef06997fd57b2b297b
Hiroshi Urabe <mail@torounit.com>
2015-08-07 14:32:15 +0900

    functions.phpを整理

--- functions.php
+++ functions.php
@@ -1,90 +1,16 @@
 <?php
 
-add_action('after_setup_theme', function(){
-	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'automatic-feed-links' );
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption'
-	) );
-	add_image_size( "plugin-image", 772, 250, true );
-
-});
-
-
-
-/*
- *  Theme Frontend Setting.
- *  ==============================================================================
- */
-
-
-
-/*
- *  Assets
- *  ----------------------------------------------------
- */
-
-function enqueue_scripts() {
-	wp_enqueue_style( 'dashicons' );
-	wp_enqueue_style( "font-awesome", "//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" );
-	wp_enqueue_style( "roboto", "http://fonts.googleapis.com/css?family=Roboto:400,300" );
-
-	if ( WP_DEBUG ) {
-		wp_enqueue_style( "all", get_template_directory_uri() . "/assets/dist/styles/all.css", array( 'dashicons' ), '1.1.1' );
-		wp_enqueue_script( 'all', get_template_directory_uri() . "/assets/dist/scripts/all.js", array( "jquery" ), '1.1.1', true );
-	} else {
-		wp_enqueue_style( "all", get_template_directory_uri() . "/assets/dist/styles/all.min.css", array( 'dashicons' ), '1.1.1' );
-		wp_enqueue_script( 'all', get_template_directory_uri() . "/assets/dist/scripts/all.min.js", array( "jquery" ), '1.1.1', true );
-	}
-
-	wp_enqueue_script( 'hatena', "//b.st-hatena.com/js/bookmark_button.js", [ ], null, true );
-
-}
+require_once "src/Assets.php";
+require_once "src/Helper.php";
+require_once "src/Support.php";
+require_once "src/Image.php";
 
 add_action('after_setup_theme', function(){
-	add_editor_style( "assets/dist/styles/all.min.css" );
-	if ( ! is_admin() ) {
-		add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
-	}
+	new Support();
+	new Assets();
+	new Image();
 
 });
-
-
-
-
-
-Class Helper {
-
-	static public function title( $paged = true ) {
-
-		if ( is_category() ) {
-			echo "Category : ";
-		}
-		if ( is_tag() ) {
-			echo "Tag : ";
-		}
-		wp_title( '', true, 'right' );
-
-		if ( $paged = true && $page_num = get_query_var( 'paged' ) ) {
-			echo " - Page " . $page_num;
-		}
-		if ( ! is_front_page() ) {
-			echo ' | ';
-		}
-	}
-
-	static public function getImageURI( $path = "" ) {
-		$theme       = get_template_directory_uri();
-		$imageDir    = ( WP_DEBUG ) ? "assets/src/images" : "assets/dist/images";
-		$imageDirURI = $theme . "/" . $imageDir;
-
-		return ( $path ) ? $theme . "/" . $imageDir . "/" . $path : $theme . "/" . $imageDir;
-	}
-}
 
 
 add_filter( "post_class", function ( $classes ) {
---
+++ src/Support.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class Support {
+
+
+	/**
+	 * Support constructor.
+	 */
+	public function __construct() {
+
+		add_theme_support( 'post-thumbnails' );
+		add_theme_support( 'automatic-feed-links' );
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption'
+		) );
+	}
+}
