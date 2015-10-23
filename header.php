1f672ecc9871e052a389930eb2a5b35ea89c74ba
torounit <mail@torounit.com>
2015-10-23 05:57:21 +0000

    Update

---
+++ header.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="ja" itemscope <?php if ( ! is_search() ): ?>itemtype="http://schema.org/WebPage"
+      <?php else: ?>itemtype="http://schema.org/SearchResultsPage"<?php endif; ?>>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0,  minimum-scale=1.0, user-scalable=yes">
+	<meta name="twitter:widgets:csp" content="on">
+	<title><?php Helper::title(); bloginfo( 'name' ); ?></title>
+	<meta http-equiv="x-dns-prefetch-control" content="on">
+	<link rel="dns-prefetch" href="//a.disquscdn.com">
+	<link rel="dns-prefetch" href="//apis.google.com">
+	<link rel="dns-prefetch" href="//pagead2.googlesyndication.com">
+	<link rel="dns-prefetch" href="//www.google-analytics.com">
+	<link rel="dns-prefetch" href="//s.gravatar.com">
+	<link rel="dns-prefetch" href="//www.facebook.com">
+	<link rel="dns-prefetch" href="//platform.twitter.com">
+	<link rel="dns-prefetch" href="//cdn-ak.b.st-hatena.com">
+	<link rel="start" title="<?php bloginfo( 'name' ); ?>" href="<?php bloginfo( 'url' ); ?>">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="shortcut icon" href="<?php bloginfo( "stylesheet_directory" ); ?>/favicon.ico"
+	      type="image/vnd.microsoft.icon"/>
+	<link rel="icon" href="<?php bloginfo( "stylesheet_directory" ); ?>/favicon.ico" type="image/vnd.microsoft.icon"/>
+	<?php wp_head(); ?>
+
+</head>
+<body>
+<?php if ( ! is_user_logged_in() ) { ?>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-18819722-1', 'torounit.com');
+		ga('require', 'displayfeatures');
+		ga('send', 'pageview');
+	</script>
+<?php } ?>
+<div class="l-viewport">
+
+	<div class="l-row l-row_tightly">
+		<div class="l-navbar">
+			<ul class="p-nav">
+				<li class="p-nav__item"><a href="#" class="js-btn" data-slidemenu-target=".js-search" title="search"><i
+							class="fa fa-search fa-lg"></i></a></li>
+				<li class="p-nav__item"><a href="#" class="js-btn" data-slidemenu-target=".js-menu" title="menu"><i
+							class="fa fa-bars fa-lg"></i></a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="js-search l-row c-slidemenu">
+		<div class="p-search">
+			<form action="/" itemprop="potentialAction" itemscope itemtype="http://schema.org/SearchAction">
+				<meta itemprop="target" content="http://www.torounit.com/?s={s}">
+				<input type="search" id="s" name="s" value="" class="p-search__input" placeholder="Search..."
+				       itemprop="query-input">
+			</form>
+		</div>
+	</div>
+
+	<div class="js-menu c-slidemenu">
+		<nav class="p-menu" itemscope itemtype="http://schema.org/SiteNavigationElement">
+			<div class="l-row l-row_tightly">
+				<ul class="p-menu__items">
+					<?php wp_list_pages( 'title_li=' ); ?>
+				</ul>
+			</div>
+		</nav>
+	</div>
+
+	<header class="l-WPHeader" itemscope itemtype="http://schema.org/WPHeader">
+		<div class="l-row">
+			<h1 class="l-WPHeader__title"><a href="/" itemprop="name"><img
+						src="<?= Helper::getImageURI( "torounit.svg" ); ?>" alt="Toro_Unit"></a></h1>
+		</div>
+	</header>
+
+	<nav class="p-breadcrumbs l-row" itemprop="breadcrumb">
+		<?php if ( function_exists( 'bread_crumb' ) ) {
+			bread_crumb( [ "type" => "string", "home_label" => '<i class="fa fa-home"></i>' ] );
+		} ?>
+	</nav>
---
+++ src/Image.php
@@ -0,0 +1,13 @@
+<?php
+
+class Image {
+
+
+	/**
+	 * Image constructor.
+	 */
+	public function __construct() {
+
+		add_image_size( "plugin-image", 772, 250, true );
+	}
+}
