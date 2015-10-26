f14fed17b637b899c0c608d1794c8f51716864a3
Hiroshi Urabe <mail@torounit.com>
2015-10-26 19:04:35 +0900

    remove google analytics

--- header.php
+++ header.php
@@ -24,18 +24,7 @@
 
 </head>
 <body>
-<?php if ( ! is_user_logged_in() ) { ?>
-	<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		ga('create', 'UA-18819722-1', 'torounit.com');
-		ga('require', 'displayfeatures');
-		ga('send', 'pageview');
-	</script>
-<?php } ?>
 <div class="l-viewport">
 
 	<div class="l-row l-row_tightly">
