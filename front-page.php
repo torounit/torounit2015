7faccbcf8577a1901852fb64447abde6d3d1e555
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-11-17 23:54:17 +0900

    bread crumb in header

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@
 
 get_header(); ?>
 
-<main class="l-main" itemscope itemtype="http://schema.org/WebPageElement" itemprop="mainContentOfPage">
+<main itemscope itemtype="http://schema.org/WebPageElement" itemprop="mainContentOfPage">
 	<?php if ( have_posts() ) : the_post(); ?>
 		<?php the_content();?>
 	<?php endif;?>
